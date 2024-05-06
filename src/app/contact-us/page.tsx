db288c3fce1240e1332f93e5e0e8f8c4421b5b54
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-05-05 23:20:46 -0500

    added page routes

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import React from 'react'
+
+export default function ContactUsPage() {
+  return (
+    <div>Contact us page</div>
+  )
+}
---
+++ src/app/get-support/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import React from 'react'
+
+export default function GetSupportPage() {
+  return (
+    <div>Get support page</div>
+  )
+}
