6c5f3ad9fab5c2329a628a85b9c9e2032505f81d
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-05-07 17:17:21 -0500

    added Header component to layout

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -3,13 +3,15 @@ import React from "react"
 import Image from "next/image"
 import Link from "next/link"
 import Box from "@mui/material/Box"
+import Navigation from "./Navigation"
 import { styled } from "@mui/material/styles"
 import parityLogo from "../../../../public/parity-logo.png"
 
 const StyledHeader = styled(Box)(
   {
-    // padding: '0px 20px',
-    // border: "2px solid lightgreen",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   ({ theme }) => ({})
 )
@@ -18,16 +20,24 @@ export default function Header() {
   return (
     <StyledHeader
       as="header"
-      sx={{ py: 2, px: { xs: "20px", md: "40px", lg: "80px" } }}
+      sx={{
+        py: { xs: "12px", lg: "20px" },
+        px: { xs: "20px", md: "40px", lg: "80px" },
+      }}
     >
-      <Link
-        href={
-          process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/"
-        }
-        style={{ padding: "6px", display: "inline-block" }}
-      >
-        <Image src={parityLogo} height={32} alt="Company logo." />
-      </Link>
+      <div>
+        <Link
+          href={
+            process.env.NODE_ENV === "development"
+              ? "http://localhost:3000"
+              : "/"
+          }
+          style={{ padding: "6px", display: "inline-block" }}
+        >
+          <Image src={parityLogo} height={32} alt="Company logo." />
+        </Link>
+      </div>
+      <Navigation />
     </StyledHeader>
   )
 }
