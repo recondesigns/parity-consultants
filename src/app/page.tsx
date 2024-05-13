8c9c00819cbef54e15f825f4567714a8fa36b8ee
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-05-12 20:46:59 -0500

    feat(): commented out unused section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <HeroSection />
       <ServicesSection />
       <ScaleComputingSection />
-      <ScaleComputingSection />
+      {/* <ScaleComputingSection /> */}
       <SocialProofSection />
       <SolutionsSection />
       <PartnersSection />
