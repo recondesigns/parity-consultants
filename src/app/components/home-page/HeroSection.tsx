4a05703dff51b6d3d1a8ed66dce5611babd9fbdf
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-05-14 15:47:21 -0500

    feat(): added button link to scale-computing section

--- src/app/components/home-page/HeroSection.tsx
+++ src/app/components/home-page/HeroSection.tsx
@@ -1,110 +1,161 @@
+"use client"
 import React from "react"
-import { Parallax } from "react-parallax"
+import Link from "next/link"
+import { styled } from "@mui/material/styles"
 import Box from "@mui/material/Box"
 import Grid from "@mui/material/Grid"
+import Button from "@mui/material/Button"
 import ScaleComputingSectionForm from "./scale-computing-section/ScaleComputingSectionForm"
 import Typography from "@mui/material/Typography"
 import { Section } from "../ui"
-import networkingImage from "../../../../public/images/blue-networking-1000.jpg"
 
-export default function HeroSection() {
+const StyledLink = styled(Link)(
+  {
+    textDecoration: "none",
+  },
+  ({ theme }) => ({
+    color: "#000000",
+    "&:hover": {
+      color: "gray",
+    },
+  })
+)
+
+type Props = {
+  includeLearnMoreButton?: boolean
+}
+
+export default function HeroSection({ includeLearnMoreButton = false }: Props) {
   return (
-    <Section>
-      <Parallax
-        bgImage={`${networkingImage.src}`}
-        bgImageAlt="networking servers"
-        strength={400}
-        style={{ width: "100%" }}
+    <Section sx={{ background: "linear-gradient(45deg, #ff512f, #f09819)" }}>
+      <Box
+        sx={{
+          padding: {
+            xs: "80px 20px",
+            sm: "100px 40px",
+            md: "100px 80px",
+            lg: "120px 100px",
+          },
+        }}
       >
-        <Box
+        <Grid
+          container
           sx={{
-            padding: {
-              xs: "80px 20px",
-              sm: "100px 40px",
-              md: "100px 80px",
-              lg: "120px 100px",
-            },
+            px: { sm: "40px", md: "0px" },
           }}
         >
           <Grid
-            container
+            item
+            xs={12}
+            md={6}
             sx={{
-              px: { sm: "40px", md: "0px" },
+              paddingBottom: { xs: "32px", md: "0px" },
+              px: { xs: "20px", md: "0px" },
+              // border: "2px solid green",
             }}
           >
-            <Grid
-              item
-              xs={12}
-              md={6}
+            <Box
               sx={{
-                paddingBottom: { xs: "32px", md: "0px" },
+                height: { md: "100%" },
+                display: { sm: "flex" },
+                flexDirection: { sm: "column" },
+                justifyContent: { sm: "center" },
+              }}
+            >
+              <Typography
+                variant="h3"
+                component="h2"
+                fontFamily="inherit"
+                fontWeight="bold"
+                color="#FDFCFC"
+                textAlign={{ xs: "center", md: "left" }}
+              >
+                Scale Computing
+              </Typography>
+              <Typography
+                variant="h6"
+                component="p"
+                fontFamily="inherit"
+                color="#FDFCFC"
+                textAlign={{ xs: "center", md: "left" }}
+                pt={{ xs: "8px", md: "0px" }}
+              >
+                Get the latest in Edge computing
+              </Typography>
+              <Typography
+                variant="body1"
+                fontFamily="inherit"
+                color="#FDFCFC"
+                textAlign={{ xs: "center", md: "left" }}
+                pt="20px"
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum numquam blanditiis
+              </Typography>
+              {includeLearnMoreButton && (
+                <Box
+                  sx={{
+                    paddingTop: {
+                      xs: "20px",
+                      sm: "24px",
+                      // md: "32px",
+                      // lg: "48px",
+                    },
+                    display: "flex",
 
-                paddingRight: { md: "12px", lg: "0px" },
-              }}
-            >
-              <Box
-                sx={{
-                  height: { md: "100%" },
-                  display: { sm: "flex" },
-                  flexDirection: { sm: "column" },
-                  justifyContent: { sm: "center" },
-                }}
-              >
-                <Typography
-                  variant="h3"
-                  component="h2"
-                  fontFamily="inherit"
-                  fontWeight="bold"
-                  color="#FDFCFC"
-                  textAlign={{ xs: "center", md: "left" }}
+                    justifyContent: { xs: "center", md: "start" },
+                    // border: "2px solid blue",
+                  }}
                 >
-                  Scale Computing
-                </Typography>
-                <Typography
-                  variant="h6"
-                  component="p"
-                  fontFamily="inherit"
-                  color="#FDFCFC"
-                  textAlign={{ xs: "center", md: "left" }}
-                  pt={{ xs: "8px", md: "0px" }}
-                >
-                  Get the latest in Edge computing
-                </Typography>
-                <Typography
-                  variant="body1"
-                  fontFamily="inherit"
-                  color="#FDFCFC"
-                  textAlign={{ xs: "center", md: "left" }}
-                  pt="20px"
-                >
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Maxime mollitia, molestiae quas vel sint commodi repudiandae
-                  consequuntur voluptatum laborum numquam blanditiis
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              md={6}
-              sx={{
-                paddingLeft: { md: "12px", lg: "0px" },
-              }}
-            >
-              <Box
-                sx={{
-                  height: { md: "100%" },
-                  display: { xs: "flex" },
-                  justifyContent: { xs: "center" },
-                  alignItems: { sm: "start", md: "center" },
-                }}
-              >
-                <ScaleComputingSectionForm />
-              </Box>
-            </Grid>
+                  <StyledLink href={"/scale-computing"}>
+                    <Button
+                      component="div"
+                      variant="outlined"
+                      size="large"
+                      sx={{
+                        color: "#FDFCFC",
+                        borderColor: "#FDFCFC",
+                        borderRadius: "50px",
+                        "&:hover": {
+                          background: "none",
+                          borderColor: "rgba(253,252,252, 0.4)",
+                        },
+                        textTransform: "none",
+                      }}
+                    >
+                      Learn more
+                    </Button>
+                  </StyledLink>
+                </Box>
+              )}
+
+              {/* <Box sx={{ border: "2px solid blue" }}>
+                <Button variant="outlined">Learn more</Button>
+              </Box> */}
+            </Box>
           </Grid>
-        </Box>
-      </Parallax>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{
+              paddingLeft: { md: "12px", lg: "0px" },
+            }}
+          >
+            <Box
+              sx={{
+                height: { md: "100%" },
+                display: { xs: "flex" },
+                justifyContent: { xs: "center" },
+                alignItems: { sm: "start", md: "center" },
+              }}
+            >
+              <ScaleComputingSectionForm />
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </Section>
   )
 }
