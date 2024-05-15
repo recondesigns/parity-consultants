dfef086e09e7105cc495403bac9e9d39e34b5f7d
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-05-15 15:37:16 -0500

    feat(): added active link styles to flyout menu

--- src/app/components/ui/header/NavigationFlyout.tsx
+++ src/app/components/ui/header/NavigationFlyout.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React from "react"
+import { usePathname } from "next/navigation"
 import Image from "next/image"
 import Link from "next/link"
 import { styled } from "@mui/material/styles"
@@ -24,72 +25,103 @@ const StyledLink = styled(Link)(
   })
 )
 
+type StyledLinkTextProps = {
+  isActiveLink?: boolean
+}
+
+const StyledLinkText = styled(Typography)<StyledLinkTextProps>(
+  {
+    color: "#333333",
+    fontFamily: "inherit",
+  },
+  ({ isActiveLink }) => ({
+    color: !isActiveLink ? "#333333" : "#D95C5C",
+    borderBottom: !isActiveLink ? "3px solid none" : "3px solid #D95C5C",
+  })
+)
+
 type Props = {
   isOpen: boolean
   onClose?: any
 }
 
-const list = (onClose: any) => (
-  <Box sx={{ width: 250 }} role="presentation">
-    <List>
-      <ListItem
-        sx={{ padding: "20px 0px", display: "flex", justifyContent: "center" }}
-      >
-        <StyledLink href="/" onClick={onClose}>
-          <Image src={parityLogo} height={40} alt="Company logo." />
-        </StyledLink>
-      </ListItem>
-      <Divider />
-      <ListItem>
-        <StyledLink href="/" onClick={onClose}>
-          <Typography sx={{ color: "#333333", fontFamily: "inherit" }}>
-            Home
-          </Typography>
-        </StyledLink>
-      </ListItem>
-      <ListItem>
-        <StyledLink href="/services" onClick={onClose}>
-          <Typography sx={{ color: "#333333", fontFamily: "inherit" }}>
-            Services
-          </Typography>
-        </StyledLink>
-      </ListItem>
-      <ListItem>
-        <StyledLink href="/solutions" onClick={onClose}>
-          <Typography sx={{ color: "#333333", fontFamily: "inherit" }}>
-            Solutions
-          </Typography>
-        </StyledLink>
-      </ListItem>
-      {/* <ListItem>
-        <StyledLink href="/scale-computing" onClick={onClose}>
-          <Typography sx={{ color: "#333333", fontFamily: "inherit" }}>
-            Scale Computing
-          </Typography>
-        </StyledLink>
-      </ListItem> */}
-      <ListItem>
-        <StyledLink href="/get-support" onClick={onClose}>
-          <Typography sx={{ color: "#333333", fontFamily: "inherit" }}>
-            Support
-          </Typography>
-        </StyledLink>
-      </ListItem>
-      <ListItem>
-        <StyledLink href="/contact-us" onClick={onClose}>
-          <Typography sx={{ color: "#333333", fontFamily: "inherit" }}>
-            Contact
-          </Typography>
-        </StyledLink>
-      </ListItem>
-    </List>
-  </Box>
-)
+function setIsActiveLink(currentPath: string, linkpath: string) {
+  return currentPath === linkpath ? true : false
+}
 
 export default function NavigationFlyout({ isOpen = false, onClose }: Props) {
+  const pathname = usePathname()
+
   return (
     <Drawer open={isOpen} anchor="right" onClose={onClose}>
-      {list(onClose)}
+      <Box sx={{ width: 250 }} role="presentation">
+        <List>
+          <ListItem
+            sx={{
+              padding: "20px 0px",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <StyledLink href="/" onClick={onClose}>
+              <Image src={parityLogo} height={40} alt="Company logo." />
+            </StyledLink>
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <StyledLink href="/" onClick={onClose}>
+              <StyledLinkText isActiveLink={setIsActiveLink(pathname, "/")}>
+                Home
+              </StyledLinkText>
+            </StyledLink>
+          </ListItem>
+          <ListItem>
+            <StyledLink href="/services" onClick={onClose}>
+              <StyledLinkText
+                isActiveLink={setIsActiveLink(pathname, "/services")}
+              >
+                Services
+              </StyledLinkText>
+            </StyledLink>
+          </ListItem>
+          <ListItem>
+            <StyledLink href="/solutions" onClick={onClose}>
+              <StyledLinkText
+                isActiveLink={setIsActiveLink(pathname, "/solutions")}
+              >
+                Solutions
+              </StyledLinkText>
+            </StyledLink>
+          </ListItem>
+          {/* <ListItem>
+            <StyledLink href="/scale-computing" onClick={onClose}>
+              <StyledLinkText
+                isActiveLink={setIsActiveLink(pathname, "/scale-computing")}
+              >
+                Scale Computing
+              </StyledLinkText>
+            </StyledLink>
+          </ListItem> */}
+          <ListItem>
+            <StyledLink href="/get-support" onClick={onClose}>
+              <StyledLinkText
+                isActiveLink={setIsActiveLink(pathname, "/get-support")}
+              >
+                Support
+              </StyledLinkText>
+            </StyledLink>
+          </ListItem>
+          <ListItem>
+            <StyledLink href="/contact-us" onClick={onClose}>
+              <StyledLinkText
+                isActiveLink={setIsActiveLink(pathname, "/contact-us")}
+              >
+                Contact
+              </StyledLinkText>
+            </StyledLink>
+          </ListItem>
+        </List>
+      </Box>
     </Drawer>
   )
 }
