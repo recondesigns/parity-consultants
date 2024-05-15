714a07f37a8ae8bb790b93abd0640812cce5ae03
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-05-15 12:54:55 -0500

    feat(): removed scrolling card styles and cards are just stacic

--- src/app/components/ui/section/MovingCardsSection.tsx
+++ src/app/components/ui/section/MovingCardsSection.tsx
@@ -1,6 +1,6 @@
 "use client"
-import { cn } from "../../../utils/cn"
 import React, { useEffect, useState } from "react"
+import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box"
 import Card from "@mui/material/Card"
 import CardContent from "@mui/material/CardContent"
@@ -10,10 +10,6 @@ import tempAvatarImage from "../../../../../public/images/temp-avatar-image.png"
 
 const MovingCardsSection = ({
   items,
-  direction = "left",
-  speed = "fast",
-  pauseOnHover = true,
-  className,
 }: {
   items: {
     quote: string
@@ -21,89 +17,29 @@ const MovingCardsSection = ({
     company: string
     testimonialImage: string
   }[]
-  direction?: "left" | "right"
-  speed?: "fast" | "normal" | "slow"
-  pauseOnHover?: boolean
-  className?: string
 }) => {
-  const containerRef = React.useRef<HTMLDivElement>(null)
-  const scrollerRef = React.useRef<HTMLUListElement>(null)
-  const [start, setStart] = useState(false)
-
-  useEffect(() => {
-    function addAnimation() {
-      if (containerRef.current && scrollerRef.current) {
-        const scrollerContent = Array.from(scrollerRef.current.children)
-
-        scrollerContent.forEach((item) => {
-          const duplicatedItem = item.cloneNode(true)
-          if (scrollerRef.current) {
-            scrollerRef.current.appendChild(duplicatedItem)
-          }
-        })
-
-        getDirection()
-        getSpeed()
-        setStart(true)
-      }
-    }
-
-    return addAnimation()
-  }, [])
-
-  const getDirection = () => {
-    if (containerRef.current) {
-      if (direction === "left") {
-        containerRef.current.style.setProperty(
-          "--animation-direction",
-          "forwards"
-        )
-      } else {
-        containerRef.current.style.setProperty(
-          "--animation-direction",
-          "reverse"
-        )
-      }
-    }
-  }
-  const getSpeed = () => {
-    if (containerRef.current) {
-      if (speed === "fast") {
-        containerRef.current.style.setProperty("--animation-duration", "20s")
-      } else if (speed === "normal") {
-        containerRef.current.style.setProperty("--animation-duration", "40s")
-      } else {
-        containerRef.current.style.setProperty("--animation-duration", "80s")
-      }
-    }
-  }
-
   return (
-    <Box
-      ref={containerRef}
-      className={cn("scroller relative z-20  max-w-full", className)}
-      sx={{ py: { xs: '8px' }, overflow: 'hidden' }}
-    >
-      <Box
-        component={"ul"}
-        ref={scrollerRef}
-        className={cn(
-          start && "animate-scroll ",
-          pauseOnHover && "hover:[animation-play-state:paused]"
-        )}
-        sx={{ width: "100%", display: "flex", gap: "12px" }}
-      >
+    <Box>
+      <Grid component={"ul"} container spacing={"20px"}>
         {items.map((item, idx) => (
-          <Box
+          <Grid
+            item
             component={"li"}
+            xs={12}
+            md={6}
+            lg={4}
             sx={{
-              minWidth: { xs: "340px", sm: "440px" },
-              maxWidth: "300px",
-              background: "#FDFCFC",
+              borderRadius: "20px",
             }}
             key={item.name}
           >
-            <Card>
+            <Card
+              sx={{
+                background: "#FDFCFC",
+                borderRadius: "20px",
+                boxShadow: "none",
+              }}
+            >
               <CardContent>
                 <Box
                   component={"blockquote"}
@@ -111,10 +47,6 @@ const MovingCardsSection = ({
                     height: "100%",
                   }}
                 >
-                  <div
-                    aria-hidden="true"
-                    className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
-                  ></div>
                   <Box>
                     <Box sx={{ padding: "8px 0px" }}>
                       <Typography variant="body1" color="#333333">
@@ -151,11 +83,11 @@ const MovingCardsSection = ({
                 </Box>
               </CardContent>
             </Card>
-          </Box>
+          </Grid>
         ))}
-      </Box>
+      </Grid>
     </Box>
   )
 }
 
-export default MovingCardsSection
+export default React.memo(MovingCardsSection)
