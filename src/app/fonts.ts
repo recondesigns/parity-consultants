9781e67c7af469776ed9c3268a56c48691ef1732
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-05-17 23:39:35 -0500

    feat(): added fonts file in app and added Montserrat font from Google

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Mulish, Montserrat } from "next/font/google";
+
+export const mulish = Mulish({
+    subsets: ['latin'],
+    display: 'swap'
+})
+
+export const montserrat = Montserrat({
+    subsets: ['latin'],
+    display: 'swap'
+})
