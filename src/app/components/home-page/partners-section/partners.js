233f3b03d4bdabe195d27906611372becd2faa3c
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-05-17 22:50:53 -0500

    feat(): added new partner logos and replaced verions with larger sizes

--- src/app/components/home-page/partners-section/partners.js
+++ src/app/components/home-page/partners-section/partners.js
@@ -1,74 +1,101 @@
-import scalelogo from '../../../../../public/images/partner-logo-scale.png'
-import barracudaLogo from '../../../../../public/images/partner-logo-barracuda.png'
-import hplogo from '../../../../../public/images/partner-logo-hp.png'
-import ingramlogo from '../../../../../public/images/partner-logo-ingram.png'
-import masergylogo from '../../../../../public/images/partner-logo-masergy.png'
-import microsoftSmallBusniesslogo from '../../../../../public/images/partner-logo-microsoft-small-business.png'
-import microsoftlogo from '../../../../../public/images/partner-logo-microsoft.png'
-import sonicwalllogo from '../../../../../public/images/partner-logo-sonicwall.png'
-import webrootlogo from '../../../../../public/images/partner-logo-webroot.png'
+import scalelogo from "../../../../../public/images/partner-logo-scale.png"
+import barracudaLogo from "../../../../../public/images/partner-logo-barracuda.png"
+import hplogo from "../../../../../public/images/partner-logo-hp.png"
+import masergylogo from "../../../../../public/images/partner-logo-masergy.png"
+import microsoftSmallBusniesslogo from "../../../../../public/images/partner-logo-microsoft-small-business.png"
+import microsoftCertifiedPartnerlogo from "../../../../../public/images/partner-logo-microsoft-certified-partner.png"
+import sonicwalllogo from "../../../../../public/images/partner-logo-sonicwall.png"
+import webrootlogo from "../../../../../public/images/partner-logo-webroot.png"
+import scaleGoldLogo from "../../../../../public/images/partner-logo-scale-gold.png"
+import epicLogo from "../../../../../public/images/partner-logo-epic.png"
+import spectrumLogo from "../../../../../public/images/partner-logo-spectrum.png"
+import lenovoLogo from "../../../../../public/images/partner-logo-lenovo.png"
+import broadvoiceLogo from "../../../../../public/images/partner-logo-broadvoice.png"
+import microsoftCertfiedLogo from "../../../../../public/images/partner-logo-microsoft-certified-bsp.png"
+import hpEnterpriseLogo from "../../../../../public/images/partner-logo-hp-enterprise-business-partner.png"
+import ingramMicroLogo from "../../../../../public/images/partner-logo-ingram-micro.png"
 
 const partners = [
-    {
-        name: 'Scale Computing',
-        image: scalelogo,
-        url: 'https://www.scalecomputing.com/'
-    },
-    {
-        name: 'Barracuda Networks',
-        image: barracudaLogo,
-        url: 'https://www.barracuda.com/'
-    },
-    // {
-    //     name: 'Drobo',
-    //     image: drobologo,
-    //     url: 'https://www.scalecomputing.com/'
-    // },
-    {
-        name: 'HP',
-        image: hplogo,
-        url: 'https://www.hp.com/us-en/home.html'
-    },
-    {
-        name: 'Masergy',
-        image: masergylogo,
-        url: 'https://www.masergy.com/'
-    },
-    {
-        name: 'Ingram Micro',
-        image: ingramlogo,
-        url: 'https://www.ingrammicro.com/'
-    },
-    {
-        name: 'Microsoft Small Business',
-        image: microsoftSmallBusniesslogo,
-        url: 'https://www.microsoft.com/en-us/'
-    },
-    {
-        name: 'Microsoft',
-        image: microsoftlogo,
-        url: 'https://www.microsoft.com/en-us/'
-    },
-    // {
-    //     name: 'Proxim Wireless',
-    //     image: proximlogo,
-    //     url: 'https://proxim.com/'
-    // },
-    {
-        name: 'Sonicewall',
-        image: sonicwalllogo,
-        url: 'https://www.sonicwall.com/'
-    },
-    {
-        name: 'Webroot',
-        image: webrootlogo,
-        url: 'https://www.webroot.com/us/en'
-    },
-    // {
-    //     name: 'VMWare',
-    //     image: vmwarelogo,
-    //     url: 'https://www.vmware.com/'
-    // },
+  {
+    name: "Scale Computing",
+    image: scalelogo,
+    url: "https://www.scalecomputing.com/",
+  },
+  {
+    name: "Scale Computing Gold Partner",
+    image: scaleGoldLogo,
+    url: "https://www.scalecomputing.com/",
+  },
+  {
+    name: "Epic iO",
+    image: epicLogo,
+    url: "https://epicio.com/",
+  },
+  {
+    name: "Spectrum VoIP",
+    image: spectrumLogo,
+    url: "https://www.spectrumvoip.com/",
+  },
+  {
+    name: "Ingram Micro",
+    image: ingramMicroLogo,
+    url: "https://www.ingrammicro.com/en-us/home",
+  },
+  {
+    name: "Lenovo",
+    image: lenovoLogo,
+    url: "https://www.lenovo.com/us/en/",
+  },
+  {
+    name: "Broadvoice",
+    image: broadvoiceLogo,
+    url: "https://www.broadvoice.com/",
+  },
+  {
+    name: "Microsoft Certified Business Solutions Partner",
+    image: microsoftCertfiedLogo,
+    url: "https://www.microsoft.com/en-us/",
+  },
+  {
+    name: "Microsoft Small Business",
+    image: microsoftSmallBusniesslogo,
+    url: "https://www.microsoft.com/en-us/",
+  },
+  {
+    name: "Microsoft Certified Partner",
+    image: microsoftCertifiedPartnerlogo,
+    url: "https://www.microsoft.com/en-us/",
+  },
+  {
+    name: "HP Enterprise Business Partner",
+    image: hpEnterpriseLogo,
+    url: "https://www.hp.com/us-en/home.html",
+  },
+  {
+    name: "HP",
+    image: hplogo,
+    url: "https://www.hp.com/us-en/home.html",
+  },
+  {
+    name: "Masergy",
+    image: masergylogo,
+    url: "https://www.masergy.com/",
+  },
+  {
+    name: "Barracuda Networks",
+    image: barracudaLogo,
+    url: "https://www.barracuda.com/",
+  },
+  {
+    name: "Sonicewall",
+    image: sonicwalllogo,
+    url: "https://www.sonicwall.com/",
+  },
+  {
+    name: "Webroot",
+    image: webrootlogo,
+    url: "https://www.webroot.com/us/en",
+  },
 ]
 
 export default partners
