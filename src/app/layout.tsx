b6fb69699584ed7fc1bde69dd1cd71d8c338b89c
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-05-19 06:37:24 -0500

    feat(): removed use client and uncommented metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
-"use client"
+// "use client"
 // import React from "react"
 import type { Metadata } from "next"
 import { mulish } from "./fonts"
 import { Header, Footer } from "./components/ui/"
 import "./globals.css"
 
-// export const metadata: Metadata = {
-//   title: "Parity Consultants",
-//   description: "Your IT solutions.",
-// }
+export const metadata: Metadata = {
+  title: "Parity Consultants",
+  description: "Your IT solutions.",
+}
 
 export default function RootLayout({
   children,
